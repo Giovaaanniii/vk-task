07d3e21ec143641e27b7680e5a8e7ced132a50b0
Иван Горбунов <muervos@muervos.muervos>
2025-05-30 15:16:55 +0300

    prepare project

---
+++ src/components/Table.tsx
@@ -0,0 +1,47 @@
+function Table() {
+  return (
+    <div>
+      <table border={10}>
+        <tbody>
+          <tr className="tableLine">
+            <th className="tableContent">1</th>
+            <th className="tableContent">2</th>
+            <th className="tableContent">3</th>
+            <th className="tableContent">4</th>
+            <th className="tableContent">5</th>
+          </tr>
+          <tr className="tableLine">
+            <th className="tableContent">1</th>
+            <th className="tableContent">2</th>
+            <th className="tableContent">3</th>
+            <th className="tableContent">4</th>
+            <th className="tableContent">5</th>
+          </tr>
+          <tr className="tableLine">
+            <th className="tableContent">1</th>
+            <th className="tableContent">2</th>
+            <th className="tableContent">3</th>
+            <th className="tableContent">4</th>
+            <th className="tableContent">5</th>
+          </tr>
+          <tr className="tableLine">
+            <th className="tableContent">1</th>
+            <th className="tableContent">2</th>
+            <th className="tableContent">3</th>
+            <th className="tableContent">4</th>
+            <th className="tableContent">5</th>
+          </tr>
+          <tr className="tableLine">
+            <th className="tableContent">1</th>
+            <th className="tableContent">2</th>
+            <th className="tableContent">3</th>
+            <th className="tableContent">4</th>
+            <th className="tableContent">5</th>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Table;
---
+++ src/pages/TablePage.tsx
@@ -0,0 +1,12 @@
+import Table from "../components/Table";
+
+function TablePage() {
+  
+  return (
+    <>
+    <Table/>
+    </>
+  );
+}
+
+export default TablePage;
